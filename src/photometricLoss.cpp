a63112bc58942e339ef9aad566d2781e2ad38738
lei <lei.cl.chn@gmail.com>
2022-10-21 10:46:18 +0200

    nothing

--- src/photometricLoss.cpp
+++ src/photometricLoss.cpp
@@ -76,14 +76,14 @@ int main(int argc, char **argv) {
 	//	depth_ref_GT=dataLoader->depth_map_ref;
 
 	// Add noise to depth image depth_ref_NS
-	Mat depth_ref_NS;
-	double Mean=0.0,StdDev=10.0;
-	AddGaussianNoise_Opencv(depth_ref,depth_ref_NS,Mean,StdDev);
+//	Mat depth_ref_NS;
+//	double Mean=0.0,StdDev=10.0;
+//	AddGaussianNoise_Opencv(depth_ref,depth_ref_NS,Mean,StdDev);
 
 	// show depth image and noise one
-	imshow("depth_ref",depth_ref);
-	imshow("depth_ref_NS",depth_ref_NS);
-	waitKey(0);
+//	imshow("depth_ref",depth_ref);
+//	imshow("depth_ref_NS",depth_ref_NS);
+//	waitKey(0);
 
 	depth_target=dataLoader->depth_map_target;
 	image_ref_baseColor= dataLoader->image_ref_baseColor;
